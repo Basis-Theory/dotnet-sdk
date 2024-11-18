d5c46e49d749b12944d457bee7808adddc0be09a
Robert G. Greathouse <robert@basistheory.com>
2024-11-18 13:11:23 -0600

    Support idempotency headers

--- src/BasisTheory.Client.Test/TestClient.cs
+++ src/BasisTheory.Client.Test/TestClient.cs
@@ -64,13 +64,12 @@ public class TestClient
     }
 
     [Test]
-    [Ignore("Idempotency Header is currently not supported in dotnet SDK")]
     public async Task ShouldSupportIdempotencyHeader()
     {
         var client = GetPrivateClient();
-        var options = new RequestOptions
+        var options = new IdempotentRequestOptions
         {
-            // IdempotencyKey = Guid.NewGuid().ToString(),
+            IdempotencyKey = Guid.NewGuid().ToString(),
         };
 
         var firstTokenId = await CreateToken(client, "6011000990139424", options);
@@ -230,7 +229,7 @@ public class TestClient
         Assert.That(token.Data.GetJsonElementValue<string>("number"), Is.EqualTo(cardNumber));
     }
 
-    private static async Task<string?> CreateToken(BasisTheory client, string cardNumber, RequestOptions? options = null)
+    private static async Task<string?> CreateToken(BasisTheory client, string cardNumber, IdempotentRequestOptions? options = null)
     {
         var token = await client.Tokens.CreateAsync(new CreateTokenRequest
         {
