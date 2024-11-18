97c7a6cb9658759689350ddc6e42ba7323a73def
Robert G. Greathouse <robert@basistheory.com>
2024-11-18 13:18:36 -0600

    Support update token content type

--- src/BasisTheory.Client.Test/TestClient.cs
+++ src/BasisTheory.Client.Test/TestClient.cs
@@ -25,9 +25,9 @@ public class TestClient
         var tokenId = await CreateToken(client, cardNumber);
         await GetAndValidateCardNumber(client, tokenId, cardNumber);
 
-        // var updateCardNumber = "4242424242424242";
-        // await UpdateTokenb(client, tokenId, cardNumber);
-        // GetAndValidateCardNumber(client, tokenId, updateCardNumber);
+        var updateCardNumber = "4242424242424242";
+        await UpdateToken(client, tokenId, updateCardNumber);
+        GetAndValidateCardNumber(client, tokenId, updateCardNumber);
 
         // Create Application
         var application = await managementClient.Applications.CreateAsync(new CreateApplicationRequest
@@ -207,7 +207,7 @@ public class TestClient
         return proxyId;
     }
 
-    private static async Task UpdateTokenb(BasisTheory client, string? tokenId, string cardNumber)
+    private static async Task UpdateToken(BasisTheory client, string? tokenId, string cardNumber)
     {
         await client.Tokens.UpdateAsync(tokenId,
             new UpdateTokenRequest
