35ea902304ac047de2076212a6f9e3dd31f32ef3
Robert Greathouse <robert@basistheory.com>
2025-01-30 15:06:30 -0600

    Add Google Pay test

--- src/BasisTheory.Client.Test/TestClient.cs
+++ src/BasisTheory.Client.Test/TestClient.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace BasisTheory.Client.Test;
@@ -573,6 +574,28 @@ public class TestClient
         Thread.Sleep(TimeSpan.FromSeconds(2)); // Required to avoid error `The webhook subscription is undergoing another concurrent operation. Please wait a few seconds, then try again.`
 
         await client.Webhooks.DeleteAsync(webhookId);
+    }
+
+    [Test]
+    public async Task ShouldSupportGooglePay()
+    {
+        var client = GetPrivateClient();
+
+        var gpayToken =
+            JsonConvert.DeserializeObject<GooglePaymentMethodToken>("{\"signature\":\"MEQCIBnz8wKrUi3qrLSn6KSrTcNIo6YcOzrfre7X49S27MrKAiBMF70q7EHe0Bw8uva77pclggSiPMRTFRFl7TZILyACOQ\\u003d\\u003d\",\"intermediateSigningKey\":{\"signedKey\":\"{\\\"keyValue\\\":\\\"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEnK9rrDl5FJalSwcoZD3qB5EYcA/sYVTH2Nbh6y/EZArFvvBRQA1eI3BIv1iZeCkBLd/A2nU1ve7xENoPOfp7+Q\\\\u003d\\\\u003d\\\",\\\"keyExpiration\\\":\\\"1737724267469\\\"}\",\"signatures\":[\"MEQCIHugFzQtVBVNizwkMhG/POcZAmRRXyeiZpt3aFwBzt5cAiBSOY4pfT4tQGWzZjkldbYkpBwWGpSasxRmlt7XPNOaLQ\\u003d\\u003d\"]},\"protocolVersion\":\"ECv2\",\"signedMessage\":\"{\\\"encryptedMessage\\\":\\\"XURDnvPAIhAKT9rARBV9RT0/yVTesT/w0UniXCJflwu2TkE54UnP7ZmWBo0gKjTJIU3j8D1Rntw2Ywr2UDLbZor+UoeZltzZOAv6iAR4MfvCLSzlh3HcjechwqZM8oxSF2iZoD2XrNqOgaYbOY1EaYoLx1JpftZDuTqSDLYa+szsoPjAUgzBO5TJZTDIa3zDNAdK3UtAPwutL1M4pTyuFhUKOC12J3RzZdaGFANbKSc8vdfqnR1hqsvsEt1sWPf2O3yty91klSA7FDckvwlKfRoNyQMDhaDkEvYUi75uxcjCRHE0Jjbj61bZriSTXiG2KWNF2OKpz7l61kgPJxCpK7A7TV3P4pBLwW7DYbRusO6FupLehxOZl9nBpVfApytCZGjaSXT7QfPpxdBv8j2VfKsodOf/dwv2Thrra9a6ZzFWsUz4l7Jbr4MCBLhXH4lSuxKrlA2Rf/CVPTgz8b88cYpEDZyqLJxDstwy74/Nl7Mjc4V7thzmdskAeYSuZXKXyyeo3BHqkguRkeagEwuHiZoem2V4W2qWOF8hYn14KY3cXXNcVA\\\\u003d\\\\u003d\\\",\\\"ephemeralPublicKey\\\":\\\"BHBDKlM3tik4o9leEkHu+875bHbORaCK7dDeXFCRmv4bzWJw/4bsvtBtaBH3SW5JXkE/6pkRYAtjFzQmHMRQYvc\\\\u003d\\\",\\\"tag\\\":\\\"Hle3Oafx5sfUc3U3sCQgV0tRPhCAvPlVLYiqvbPyTYY\\\\u003d\\\"}\"}");
+
+        try
+        {
+            await client.Googlepay.TokenizeAsync(new GooglePayTokenizeRequest
+            {
+                GooglePaymentMethodToken = gpayToken
+            });
+            Assert.Fail("Should have raised an exception");
+        }
+        catch (UnprocessableEntityError e)
+        {
+            Assert.True(e.Body.Detail.Contains("expired intermediateSigningKey"));
+        }
     }
 
     private static async Task<string> UpdateWebhook(BasisTheory client, string webhookId)
